81b6700e07f655caadab6842cab1a33ebbae2265
Ilya Putilin <putilya16@gmail.com>
2023-07-30 18:02:20 +0700

    Fix the double_linked_list problems

--- double_linked_list.c
+++ double_linked_list.c
@@ -1,22 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "double_linked_list.h"
 
-
-struct page_t {
-	int num;
-};
-
-struct list_node_t {
-	struct page_t *data;
-	struct list_node_t *next;
-	struct list_node_t *prev;
-};
-
-struct list_t {
-	struct list_node_t *top;
-	struct list_node_t *back;
-};
-
+struct page_t *page_create(int num)
+{
+	struct page_t *p_page = malloc(sizeof(struct page_t));
+	p_page->num = num;
+	return p_page;
+}
 
 struct list_node_t *node_create(struct page_t *p)
 {
@@ -44,59 +35,106 @@ struct list_t *list_create()
 
 int list_size(const struct list_t *lst)
 {
-	if (lst->top == NULL) {
+	if (!lst->top) {
 		return 0;
 	}
 
 	int size = 1;
 	struct list_node_t *tmp = lst->top;
-	while (tmp->next != NULL) {
-		tmp = tmp->next;
+	while (tmp->prev != NULL) {
+		tmp = tmp->prev;
 		size++;
 	}
+
 	return size;
 }
 
-struct page_t *list_back_(struct list_t *lst)
+struct page_t *list_back(struct list_t **lst)
 {
-	return lst->back->data;
+	if (!(*lst)->top) {
+		return 0;
+	}
+	return (*lst)->back->data;
 }
 
 void list_pop_back(struct list_t *lst)
 {
-	lst->back->prev->next = NULL;
-	lst->back = lst->back->prev;
+	if (!lst->back) {
+		// printf("List is empty\n");
+		return;
+	}
+	struct list_node_t *tmp = lst->back;
+	if (!lst->top) {
+		return;
+	} else if (lst->top != lst->back) {
+		lst->back->next->prev = NULL;
+		lst->back = lst->back->next;
+	} else {
+		lst->back = NULL;
+		lst->top = NULL;
+	}
+	free(tmp->data);
+	free(tmp);
 }
 
-void list_push_front_(struct list_t *lst, struct page_t *q)
+void list_push_front(struct list_t *lst, struct page_t *q)
 {
-	struct list_node_t *new_node = malloc(sizeof(struct list_node_t));
-	new_node->data = q;
-	new_node->next = lst->top;
-	new_node->prev = NULL;
+	struct list_node_t *new_node = node_create(q);
+	if (lst->top) {
+		// list is not empty
+		lst->top->next = new_node;
+		new_node->prev = lst->top;
+	} else {
+		// list is empty
+		lst->back = new_node;
+	}
 	lst->top = new_node;
 }
 
-void list_move_upfront_(struct list_t *lst, struct list_node_t *p)
+void list_move_upfront(struct list_t *lst, struct list_node_t *p)
 {
-	p->prev->next = p->next;
-	p->next->prev = p->prev;
-	lst->top->prev = p;
-	p->next = lst->top;
-	p->prev = NULL;
+	if (lst->top == lst->back || !p->next) {
+		return;
+	} else if (!p->prev) {
+		lst->back = p->next;
+		lst->back->prev = NULL;
+	} else {
+		p->prev->next = p->next;
+		p->next->prev = p->prev;
+	}
+	p->next = NULL;
+	p->prev = lst->top;
 	lst->top = p;
+	lst->top->prev->next = lst->top;
 }
 
-void list_free(struct list_t *lst)
+void list_free(struct list_t **lst)
 {
-	if (lst->top == NULL) {
+	if (!(*lst)->top) {
+		return;
+	}
+	struct list_node_t *tmp = (*lst)->top;
+	while ((*lst)->top) {
+		tmp = (*lst)->top;
+		(*lst)->top = (*lst)->top->prev;
+		node_free(tmp);
+	}
+	// we will free the list at the end of the programm
+	// free(*lst);
+	// *lst = NULL;
+}
+
+void list_print(const struct list_t *lst)
+{
+	if (!lst->top){
+		printf("List is empty\n");
 		return;
 	}
 	struct list_node_t *tmp = lst->top;
-	while (lst->top != NULL) {
-		tmp = lst->top;
-		lst->top = lst->top->next;
-		node_free(tmp);
+	while (tmp) {
+		printf("%d ", tmp->data->num); 
+		tmp = tmp->prev;
 	}
+	printf("\n");
 }
 
--- double_linked_list.h
+++ double_linked_list.h
@@ -1,12 +1,29 @@
 #ifndef _DOUBLE_LINEKED_LIST_
 #define _DOUBLE_LINEKED_LIST_
 
+struct page_t {
+	int num;
+};
+
+struct list_node_t {
+	struct page_t *data;
+	struct list_node_t *next;
+	struct list_node_t *prev;
+};
+
+struct list_t {
+	struct list_node_t *top;
+	struct list_node_t *back;
+};
+
 struct list_t *list_create();
 int list_size(const struct list_t *lst);
-struct page_t *list_back_(struct list_t *lst);
+struct page_t *list_back(struct list_t **lst);
 void list_pop_back(struct list_t *lst);
-void list_push_front_ (struct list_t *lst, struct page_t *q);
-void list_move_upfront_(struct list_t *lst, struct list_node_t *p);
-void list_free(struct list_t *lst);
+void list_push_front(struct list_t *lst, struct page_t *q);
+void list_move_upfront(struct list_t *lst, struct list_node_t *p);
+void list_free(struct list_t **lst);
+struct page_t *page_create(int num);
+void list_print(const struct list_t *lst);
 
 #endif
